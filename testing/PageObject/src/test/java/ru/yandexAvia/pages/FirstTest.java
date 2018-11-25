fad19972f65c5812c4a4a89511c3a4dec86779fd
sollent <sollent98@gmail.com>
2018-11-25 22:13:53 +0300

    Add PageObject lab with .gitignore

---
+++ testing/PageObject/src/test/java/ru/yandexAvia/pages/FirstTest.java
@@ -0,0 +1,33 @@
+package ru.yandexAvia.pages;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FirstTest extends BaseTest{
+
+    public static SearchPage searchPage;
+
+    @BeforeClass
+    public static void initial(){
+        searchPage = new SearchPage(driver);
+        driver.get("https://avia.yandex.ru");
+    }
+
+    @Test
+    public void ticketsSearchWithValidData() throws InterruptedException {
+        searchPage.typeInputFrom("Минск");
+        searchPage.typeInputTo("Нижний Новгород");
+        searchPage.clickOnCalendar();
+        Thread.sleep(1000);
+        searchPage.selectDepartureDate("27", "10");
+        Thread.sleep(1000);
+        searchPage.selectArrivalDate("21", "11");
+        Thread.sleep(1000);
+        searchPage.clickOnPeople();
+        Thread.sleep(1000);
+        searchPage.addAdults(4);
+        Thread.sleep(500);
+        searchPage.clickOnSearch();
+    }
+
+}
