83171b4f86f340bd7e7099bf294ef25d0eab317e
sollent <sollent98@gmail.com>
2018-11-25 21:16:10 +0300

    Add test_cases.txt file and webdriver lab

--- testing/WebDriver/src/test/java/ru/utair/FirstTest.java
+++ testing/WebDriver/src/test/java/ru/utair/FirstTest.java
@@ -1,15 +1,43 @@
 package ru.utair;
 
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 
 public class FirstTest extends BaseTest {
 
+    // First test from test_cases.txt
     @Test
-    public void openPageTest(){
-        driver.get("https://www.utair.ru/");
+    public void ticketsSearchTest() throws InterruptedException {
+        driver.get("https://avia.yandex.ru");
+        Thread.sleep(1000);
 
-        String title = driver.getTitle();
-        System.out.println(title);
+        WebElement inputFrom = driver.findElementById("from");
+        WebElement inputTo = driver.findElementById("to");
+        WebElement calendar = driver.findElementByClassName("datepicker-inputs_kb");
+        WebElement peoples = driver.findElementByClassName("form-options-picker_kb__input");
+        WebElement plus = driver.findElementByClassName("passengers_kb__increase");
+        WebElement searchButton = driver.findElementByCssSelector("div.search-form_kb__button-find>button.y-button");
+
+        inputFrom.clear();
+        inputTo.clear();
+        Thread.sleep(500);
+        inputFrom.sendKeys("Минск");
+        inputTo.sendKeys("Москва");
+        Thread.sleep(1500);
+        calendar.click();
+        WebElement day = driver.findElementByCssSelector("div[data-date='27'][data-month='10']");
+        Thread.sleep(1000);
+        day.click();
+        Thread.sleep(2000);
+        peoples.click();
+        Thread.sleep(500);
+        for (int i = 0; i < 3; i++){
+            plus.click();
+            Thread.sleep(1000);
+        }
+        Thread.sleep(3000);
+        searchButton.click();
+        Thread.sleep(5000);
     }
 
 }
